56a38067939256aa90437e6dd1e802132a8bd29e
JhanCarlos Marte Jimenez <jhancarlos.jimenez@gmail.com>
2014-11-11 07:51:18 +0100

    Initial Commit

--- app/src/main/java/com/jhancarlos/consumingbookswebservice/LibraryBooks.java
+++ app/src/main/java/com/jhancarlos/consumingbookswebservice/LibraryBooks.java
@@ -1,7 +1,6 @@
 package com.jhancarlos.consumingbookswebservice;
 
 import android.app.Activity;
-import android.app.ActionBar;
 import android.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -9,12 +8,11 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.os.Build;
 
 
 
 public class LibraryBooks extends Activity {
-
+    /*Consuming Web Services*/
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
